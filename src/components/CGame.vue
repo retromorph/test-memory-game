41435a9666b08fc71c6373f3cd5de95335a9d7c7
retromorph <musalov2@gmail.com>
2021-09-14 20:10:48 +0300

    base components

---
+++ src/components/CGame.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="c-game">
+    <CCard v-for="card in arrangement"
+           :card-number="card" />
+  </div>
+</template>
+
+<script lang="ts">
+import CCard from "@/components/ui/CCard.vue"
+
+export default {
+  name: "c-game",
+  props: {
+    arrangement: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: { CCard },
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "~@/assets/styles/variables.styl"
+@import "~@/assets/styles/mixins.styl"
+
+.c-game
+  display grid
+  grid-template-columns repeat(6, 1fr)
+  grid-gap 16px
+</style>
---
+++ src/components/CResultTable.vue
@@ -0,0 +1,23 @@
+<template>
+  <table>
+    <tr v-for="result in results">
+
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "c-result-table",
+  props: {
+    results: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

e1083a0c88c42acda1bcf4d7d438bd6a53b3be06
retromorph <musalov2@gmail.com>
2021-09-14 19:47:30 +0300

    connect components

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 import "./registerServiceWorker"
+import components from "@/components/ui"
 import router from "./router"
 import store from "./store"
 
-createApp(App).use(store).use(router).mount("#app")
+const app = createApp(App)
+
+components.forEach((component) => {
+  app.component(component.name, component)
+})
+
+app.use(store).use(router).mount("#app")
